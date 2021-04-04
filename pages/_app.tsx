bd2eb3d9a8f1c8f4db609cbc875d13ec79c1da15
Adam Bergman <adam@fransvilhelm.com>
2021-04-04 15:38:32 +0200

    Add inter typeface

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
-import { AppProps } from 'next/app';
+import 'styles/main.css';
 import 'tailwindcss/tailwind.css';
+
+import { AppProps } from 'next/app';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
