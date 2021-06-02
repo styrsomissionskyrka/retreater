4a23eac8bc305b20cb1c8ef7cc83c5b1a3ff8c0e
Adam Bergman <adam@fransvilhelm.com>
2021-06-02 23:38:36 +0200

    Init auth0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,21 @@
 import 'styles/main.css';
 
+import { Fragment } from 'react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Head>
+        <title key="title">Retreater | Styrsö Missionskyrka</title>
+      </Head>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </Fragment>
+  );
 };
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
-import { Fragment } from 'react';
 import { NextPage } from 'next';
-import Head from 'next/head';
+import { useUser } from '@auth0/nextjs-auth0';
 
 const Home: NextPage = () => {
-  return (
-    <Fragment>
-      <Head>
-        <title key="title">Retreater | Styrsö Missionskyrka</title>
-      </Head>
-      <div />
-    </Fragment>
-  );
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  if (user) {
+    console.log(user);
+    return (
+      <div>
+        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+      </div>
+    );
+  }
+
+  return <a href="/api/auth/login">Login</a>;
 };
 
 export default Home;
