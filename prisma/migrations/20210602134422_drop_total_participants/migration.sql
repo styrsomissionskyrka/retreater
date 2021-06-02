6abd0a7ee4b16bf23c033104cccb32a19c9354ca
Adam Bergman <adam@fransvilhelm.com>
2021-06-02 17:43:38 +0200

    Remove totalParticipants column

---
+++ prisma/migrations/20210602134422_drop_total_participants/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `totalParticipants` on the `Retreat` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Retreat` DROP COLUMN `totalParticipants`;
