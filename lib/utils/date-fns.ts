ae447f11cd473f29c791fee31f623b5e2a87ae93
Adam Bergman <adam@fransvilhelm.com>
2021-06-07 21:37:37 +0200

    Ensure proper locale on date-fns

--- lib/utils/date-fns.ts
+++ lib/utils/date-fns.ts
@@ -1,31 +1,72 @@
 import {
+  differenceInCalendarWeeks as _differenceInCalendarWeeks,
+  eachWeekOfInterval as _eachWeekOfInterval,
+  endOfWeek as _endOfWeek,
   format as _format,
   formatDistance as _formatDistance,
   formatDistanceStrict as _formatDistanceStrict,
+  formatDistanceToNow as _formatDistanceToNow,
+  formatDistanceToNowStrict as _formatDistanceToNowStrict,
+  formatDuration as _formatDuration,
   formatRelative as _formatRelative,
+  getWeek as _getWeek,
+  getWeekOfMonth as _getWeekOfMonth,
+  getWeeksInMonth as _getWeeksInMonth,
+  getWeekYear as _getWeekYear,
+  isMatch as _isMatch,
+  isSameWeek as _isSameWeek,
+  isThisWeek as _isThisWeek,
+  lastDayOfWeek as _lastDayOfWeek,
+  parse as _parse,
+  setDay as _setDay,
+  setWeek as _setWeek,
+  setWeekYear as _setWeekYear,
+  startOfWeek as _startOfWeek,
+  startOfWeekYear as _startOfWeekYear,
 } from 'date-fns';
 import { sv } from 'date-fns/locale';
 
 export * from 'date-fns';
 
-export function format(...args: Parameters<typeof _format>): ReturnType<typeof _format> {
-  let [date, format, options] = args;
-  return _format(date, format, { locale: sv, weekStartsOn: 1, ...options });
-}
+export const differenceInCalendarWeeks = wrap(_differenceInCalendarWeeks);
+export const eachWeekOfInterval = wrap(_eachWeekOfInterval);
+export const endOfWeek = wrap(_endOfWeek);
+export const format = wrap(_format);
+export const formatDistance = wrap(_formatDistance);
+export const formatDistanceStrict = wrap(_formatDistanceStrict);
+export const formatDistanceToNow = wrap(_formatDistanceToNow);
+export const formatDistanceToNowStrict = wrap(_formatDistanceToNowStrict);
+export const formatDuration = wrap(_formatDuration);
+export const formatRelative = wrap(_formatRelative);
+export const getWeek = wrap(_getWeek);
+export const getWeekOfMonth = wrap(_getWeekOfMonth);
+export const getWeeksInMonth = wrap(_getWeeksInMonth);
+export const getWeekYear = wrap(_getWeekYear);
+export const isMatch = wrap(_isMatch);
+export const isSameWeek = wrap(_isSameWeek);
+export const isThisWeek = wrap(_isThisWeek);
+export const lastDayOfWeek = wrap(_lastDayOfWeek);
+export const parse = wrap(_parse);
+export const setDay = wrap(_setDay);
+export const setWeek = wrap(_setWeek);
+export const setWeekYear = wrap(_setWeekYear);
+export const startOfWeek = wrap(_startOfWeek);
+export const startOfWeekYear = wrap(_startOfWeekYear);
 
-export function formatDistance(...args: Parameters<typeof _formatDistance>): ReturnType<typeof _formatDistance> {
-  let [date, baseDate, options] = args;
-  return _formatDistance(date, baseDate, { locale: sv, ...options });
-}
+function wrap<T extends (...args: any[]) => any>(fn: T): T {
+  let wrapped = (...args: Parameters<T>) => {
+    let length = fn.length;
+    let newArgs = new Array(length);
+    for (let i = 0; i < length; i++) {
+      if (i === length - 1) {
+        newArgs[i] = { locale: sv, weekStartsOn: 1, ...args[i] };
+      } else {
+        newArgs[i] = args[i];
+      }
+    }
 
-export function formatDistanceStrict(
-  ...args: Parameters<typeof _formatDistanceStrict>
-): ReturnType<typeof _formatDistanceStrict> {
-  let [date, baseDate, options] = args;
-  return _formatDistanceStrict(date, baseDate, { locale: sv, ...options });
-}
+    return fn(...newArgs);
+  };
 
-export function formatRelative(...args: Parameters<typeof _formatRelative>): ReturnType<typeof _formatRelative> {
-  let [date, baseDate, options] = args;
-  return _formatRelative(date, baseDate, { locale: sv, weekStartsOn: 1, ...options });
+  return wrapped as T;
 }
