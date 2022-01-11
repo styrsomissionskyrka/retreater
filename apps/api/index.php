04667deae2789c47608b7abaf44ed9ceaaea0d02
Adam Bergman <adam@fransvilhelm.com>
2022-01-11 20:43:10 +0100

    Setup api wp theme

---
+++ apps/api/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Styrsö Missionskyrka</title>
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+  <?php wp_body_open(); ?>
+
+  <h1><?php __('Hello world', 'smk'); ?></h1>
+
+  <?php wp_footer(); ?>
+</body>
+</html>
