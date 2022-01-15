6bd90a6267bd7c224ba8ee797fb338ebe54ca0fd
Adam Bergman <adam@fransvilhelm.com>
2022-01-15 17:08:18 +0100

    Improve api dev env

--- apps/api/scripts/sync.mjs
+++ apps/api/scripts/sync.mjs
@@ -1,10 +1,16 @@
-import './package.mjs';
 import dotenv from 'dotenv';
 import fs from 'fs-extra';
 import * as path from 'path';
 
 dotenv.config({ path: '.env.local' });
-await fs.copy(
-  './package/styrsomissionskyrka',
-  path.join(process.env.SYNC_PATH, '/styrsomissionskyrka'),
-);
+
+const ignore = ['.swc', '.turbo', 'node_modules', 'package', 'scripts', 'src'];
+
+let src = path.join(process.cwd(), '.');
+let dest = path.join(process.env.SYNC_PATH, '/styrsomissionskyrka');
+
+let items = await fs.readdir(src);
+for (let item of items) {
+  if (item.startsWith('.') || ignore.includes(item)) continue;
+  await fs.copy(path.join(src, item), path.join(dest, item));
+}
