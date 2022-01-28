f61ac37b3d5dffde07e733b5256b296fbe78c9b1
Adam Bergman <adam@fransvilhelm.com>
2022-01-28 13:18:34 +0100

    Rename blocks field in api response (#2)

--- apps/retreater/pages/[slug].tsx
+++ apps/retreater/pages/[slug].tsx
@@ -5,7 +5,7 @@ import { Post } from '@styrsomissionskyrka/types';
 import { BlockRenderer } from '../components';
 import * as wp from '../lib/api/wp';
 
-const keys = ['id', 'title', 'smk_blocks'] as const;
+const keys = ['id', 'title', 'content_blocks'] as const;
 
 type Props = {
   post: Pick<Post, typeof keys[number]>;
@@ -19,7 +19,7 @@ const Retreat: NextPage<Props> = ({ post }) => {
   return (
     <article>
       <h1>{post.title.rendered}</h1>
-      <BlockRenderer blocks={post.smk_blocks} />
+      <BlockRenderer blocks={post.content_blocks} />
     </article>
   );
 };
@@ -39,7 +39,7 @@ export const getStaticProps: GetStaticProps<Props, Query> = async (context) => {
   let id = context.params?.slug;
   if (id == null) return { notFound: true };
 
-  let pick = { id: true, title: true, smk_blocks: true, status: true } as const;
+  let pick = { id: true, title: true, content_blocks: true, status: true } as const;
   let revision = context.preview ? getRevisionId(context) : null;
 
   let post = revision != null ? await wp.revision({ id, revision }, pick) : await wp.post({ id }, pick);
